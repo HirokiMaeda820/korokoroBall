095ef2a710bb73885943ca9b00a7d4c00aa8d812
HirokiMaeda <k021g1241@g.neec.ac.jp>
2022-05-16 09:03:09 +0900

    5/16

---
+++ EN1 0411/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float powor = 10;
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, 1) * powor);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(new Vector3(-1,0,0) * powor);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -1) * powor);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector3(1, 0, 0) * powor);
+        }
+    }
+}
